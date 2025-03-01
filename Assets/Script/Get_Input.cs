43b81c7157463fb4d52783edde6011de93733bb6
019NinNinJA <ninja.tananat@gmail.com>
2025-03-01 21:05:50 +0700

    InputSystem

--- Assets/Script/Get_Input.cs
+++ Assets/Script/Get_Input.cs
@@ -1,21 +1,59 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Get_Input : MonoBehaviour
 {
-    Input_handle _input_Handle;
-    Vector2Int direction;
+    private Input_handle _input_Handle;
+    private Vector2Int direction;
+    [SerializeField] private InputActionReference playerControls; 
 
+    private void Awake()
+    {
+        SetUp();
+    }
+
+    private void OnEnable()
+    {
+        if (playerControls == null || playerControls.action == null)
+        {
+            Debug.LogError("PlayerControls is not assigned or has no action in Get_Input.");
+            return;
+        }
+
+        playerControls.action.Enable();
+
+        playerControls.action.performed += OnMove;  
+        playerControls.action.canceled += OnMove;   
+    }
+
+    private void OnDisable()
+    {
+        if (playerControls != null && playerControls.action != null)
+        {
+            playerControls.action.performed -= OnMove;
+            playerControls.action.canceled -= OnMove;
+            playerControls.action.Disable();
+        }
+    }
 
     void SetUp()
     {
-        _input_Handle = this.GetComponent<Input_handle>();
+        _input_Handle = GetComponent<Input_handle>();
+
+        if (_input_Handle == null)
+        {
+            Debug.LogError("Input_handle component is missing from " + gameObject.name);
+        }
     }
 
-
-    void Update()
+    private void Update()
     {
         _input_Handle?.Calling(direction);
     }
 
-
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+        direction = new Vector2Int(Mathf.RoundToInt(input.x), Mathf.RoundToInt(input.y));
+    }
 }
