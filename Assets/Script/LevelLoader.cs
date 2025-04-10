eaeb874a4cb727d5543210db1954536eb7604170
Rubysword196 <proeyroro@gmail.com>
2025-04-10 23:15:14 +0700

    LoadScreen Added

--- Assets/Script/LevelLoader.cs
+++ Assets/Script/LevelLoader.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class LevelLoader : MonoBehaviour
 {
     [SerializeField] Get_Input get_Input;
     [SerializeField] GameObject inputGameObject;
-    
+    UnityEngine.UI.Slider loadSceneProgressBar;
+    GameObject loadScene;
+    GameObject loadSceneCanva;
+    bool LoadTimeChecker;
+    private void Awake()
+    {
+        this.transform.parent = null;
+        DontDestroyOnLoad(this.gameObject);
+    }
 
     public void LoadLevel(int levelNumber)
     {
@@ -22,7 +32,8 @@ public class LevelLoader : MonoBehaviour
         if (get_Input == null)
             Debug.LogWarning("[LevelLoader] get_Input is not assigned. Input will not be disabled.");
 
-        LoadLevel(_currentLevel + 1);
+        //LoadLevel(_currentLevel + 1);
+        StartCoroutine(ASyncLoadScene(_currentLevel + 1));
     }
     public void loadPreviousScene()
     {
@@ -33,7 +44,8 @@ public class LevelLoader : MonoBehaviour
             Debug.LogWarning("[LevelLoader] get_Input is not assigned. Input will not be disabled.");
 
 
-        LoadLevel(_currentLevel - 1);
+        //LoadLevel(_currentLevel - 1);
+        StartCoroutine(ASyncLoadScene(_currentLevel - 1));
     }
     public void reloadScene()
     {
@@ -44,6 +56,46 @@ public class LevelLoader : MonoBehaviour
             Debug.LogWarning("[LevelLoader] get_Input is not assigned. Input will not be disabled.");
 
 
-        LoadLevel(_currentLevel);
+        //LoadLevel(_currentLevel);
+        StartCoroutine(ASyncLoadScene(_currentLevel));
+    }
+
+    IEnumerator ASyncLoadScene(int levelNumber)
+    {
+        loadScene = FindAnyObjectByType<Sceneloader>(FindObjectsInactive.Include).gameObject;
+        loadSceneCanva = loadScene.transform.parent.gameObject;
+        loadSceneProgressBar = loadScene.GetComponentInChildren<UnityEngine.UI.Slider>(true);
+        LoadTimeChecker = false;
+        loadScene.SetActive(true);
+        loadSceneProgressBar.value = 0f;
+        AsyncOperation asyncload = SceneManager.LoadSceneAsync(levelNumber);
+        StartCoroutine(CheckLoadTime());
+        while (!asyncload.isDone)
+        {
+            loadSceneProgressBar.value = asyncload.progress;
+            yield return null;
+        }
+        if (LoadTimeChecker)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        else
+        {
+            yield return new WaitForSeconds(1.5f);
+        }
+        Destroy(loadSceneCanva);
+        Destroy(gameObject);
+    }
+
+    public IEnumerator CheckLoadTime(float countdownValue = 1.5f)
+    {
+        float currCountdownValue = countdownValue;
+        while (currCountdownValue > 0)
+        {
+            //Debug.Log("Countdown: " + currCountdownValue);
+            yield return new WaitForSecondsRealtime(0.1f);
+            currCountdownValue -= 0.1f;
+        }
+        LoadTimeChecker = true;
     }
 }
