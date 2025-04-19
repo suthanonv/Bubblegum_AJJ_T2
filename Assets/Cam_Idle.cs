7fe6047e1abd3e8e341dcafbb0b9f9d2236f5476
suthanonv <122354357+suthanonv@users.noreply.github.com>
2025-04-19 16:27:25 +0700

    [Import] Cam Idle Script

---
+++ Assets/Cam_Idle.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using UnityEngine;
+public class Cam_Idle : MonoBehaviour
+{
+
+
+
+    [SerializeField] Vector2 RangeX = new Vector2();
+    [SerializeField] Vector2 RangeY = new Vector2();
+    Vector2 StatPostion = new Vector2();
+    Vector2 Destination;
+
+    [SerializeField] float Period = 0.25f;
+    private void Awake()
+    {
+        StatPostion = this.transform.position;
+        Destination = GetDestination();
+
+
+    }
+
+
+    bool onRun;
+    private void Update()
+    {
+
+        if ((Vector2)this.transform.position != Destination)
+        {
+            if (onRun == false)
+            {
+                onRun = true;
+                StartCoroutine(LerpPosition(this.transform, Destination, Period));
+            }
+        }
+        else
+        {
+            Destination = GetDestination();
+        }
+    }
+
+    IEnumerator LerpPosition(Transform obj, Vector2 end, float time)
+    {
+        Vector2 start = obj.position;
+        float elapsed = 0f;
+
+        while (elapsed < time)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / time);
+            obj.position = Vector2.Lerp(start, end, t);
+            obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.localPosition.y, -10);
+            yield return null;
+        }
+
+        obj.position = end; // Snap to exact final position
+        obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.localPosition.y, -10);
+
+        onRun = false;
+
+    }
+
+    Vector2 GetDestination()
+    {
+        float x = Random.Range(-RangeX.x, RangeX.y);
+        float y = Random.Range(-RangeY.y, RangeY.y);
+
+        return StatPostion + new Vector2(x, y);
+    }
+
+
+
+
+
+}
