972c51d20947c786e068ccde8583acdc8759365b
019NinNinJA <ninja.tananat@gmail.com>
2025-04-20 15:55:15 +0700

    update sound manager

--- Assets/Script/SoundManager/VolumeSettings.cs
+++ Assets/Script/SoundManager/VolumeSettings.cs
@@ -7,16 +7,20 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] AudioMixer audioMixer;
     [SerializeField] Slider musicSlider;
     [SerializeField] Slider sfxSlider;
+    [SerializeField] Slider MasterSlider;
 
     const string MIXER_MUSIC = "MusicVolumn";
     const string MIXER_SFX = "SFXVolumn";
+    const string MIXER_MASTER = "MASTERVolumn";
 
 
     private void Awake()
     {
+        MasterSlider.onValueChanged.AddListener(SetMasterVolume);
         musicSlider.onValueChanged.AddListener(SetMusicVolume);
-        musicSlider.onValueChanged.AddListener(SetSFXVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume); 
     }
+
     private void SetMusicVolume(float value)
     {
         audioMixer.SetFloat(MIXER_MUSIC, Mathf.Log10(value) * 20);
@@ -24,5 +28,9 @@ public class VolumeSettings : MonoBehaviour
     private void SetSFXVolume(float value)
     {
         audioMixer.SetFloat(MIXER_SFX, Mathf.Log10(value) * 20);
+    }
+    private void SetMasterVolume(float value)
+    {
+        audioMixer.SetFloat(MIXER_MASTER, Mathf.Log10(value) * 20);
     }
 }
